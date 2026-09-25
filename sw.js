01b0fb051f3393ece1d469b084f4bf33f7ba1d09
proedgemetals <kajones270@yahoo.com>
2026-09-25 17:58:09 -0500

    Refresh cache for v6.2 JavaScript fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='pe-v6-1';
+const C='pe-v6-2';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','manifest.json'])))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{let copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));return}e.respondWith(fetch(e.request).then(res=>{let copy=res.clone();caches.open(C).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match(e.request)))});
